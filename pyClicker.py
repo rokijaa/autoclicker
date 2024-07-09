42cf8dc72df7e929815b6f8d8e6b0d5d9e603f69
rokija <rokijagames@gmail.com>
2024-07-09 18:22:17 +0200

    tweaked getting button input

--- pyClicker.py
+++ pyClicker.py
@@ -7,33 +7,20 @@ root = Tk()
 root.resizable(False, False)
 root.geometry("400x200")
 
-startKey = ""
-
-# Functions
-def on_key_press(x):
-    global startKey
-    startKey = x
-    print(x)
-    if hasattr(x, "char") and x.char == "z":
-        print("Z Pressed")
-    keyboard_listener.stop()
-
-keyboard_listener = keyboard.Listener(
-    on_press=on_key_press
-)
-
+def getButton():
+    if event.key == "None":
+        return
+    button.config(text=event)
 
 def onClick():
-    global startKey
-
-    button.config(text="press any key")
-
-    keyboard_listener.start()
-    time.sleep(5)
-
-    print("zmačkle")
-    button.config(text=startKey)
-
+    with keyboard.Events() as events:
+        global event
+        for event in events:
+            if event.key == keyboard.Key.esc:
+                break
+            else:
+                getButton()
+                break
 
 # Widgets
 label = Label(root, text="pyAutoclicker")
