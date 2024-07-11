607a093cf40e30a79750c955e44d198281edf686
rokija <rokijagames@gmail.com>
2024-07-11 17:43:51 +0200

    gui changes

--- pyClicker.py
+++ pyClicker.py
@@ -9,7 +9,9 @@ root.geometry("400x200")
 def getButton():
     if event.key == "None":
         return
-    button.config(text=event)
+    key = event.key
+    button.config(text=key)
+    upButtonLabel.config(text="")
 
 def onClick():
     with keyboard.Events() as events:
@@ -22,13 +24,16 @@ def onClick():
                 break
 
 # Widgets
-label = Label(root, text="pyAutoclicker")
-button = Button(root, text="Nothing", command=onClick)
+label = Label(root, text="Click button and then select a key, which will be used to start autoclicking")
+upButtonLabel = Label(root, text="! Key empty !")
+button = Button(root, text="", command=onClick)
 errorLabel = Label(root, text="")
 
+button.config(width=8, height=2)
 
 # Positioning
 label.place(x=200, y=0, anchor=N)
+upButtonLabel.place(x=200, y=30, anchor=N)
 button.place(x=200, y=50, anchor=N)
 errorLabel.place(x=10, y=20)
 
